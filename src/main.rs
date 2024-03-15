b5d8edd18992f6f78e2bbc33da8df14cfd440841
3xp1o1t <jes.monti.503@gmail.com>
2024-03-14 22:18:40 -0600

    Add CRUD handlers for notes

--- src/main.rs
+++ src/main.rs
@@ -45,14 +45,3 @@ async fn main() {
         .await
         .unwrap();
 }
-
-pub async fn health_check_handler() -> impl IntoResponse {
-    const MESSAGE: &str = "API Services";
-
-    let json_response = serde_json::json!({
-        "status":"ok",
-        "message": MESSAGE
-    });
-
-    Json(json_response)
-}
